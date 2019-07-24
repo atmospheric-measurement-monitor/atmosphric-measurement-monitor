a2d63443230dc5deb71a70d3a0f383706fb62607
c19981127 <1365902565@qq.com>
2019-07-24 08:58:40 +0800

    "update mainwindow"

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,29 +1,76 @@
 #include "mainwindow.h"
 mainwindow::mainwindow()
 {
-    main_window = new QSplitter(Qt::Vertical,0);
-    main_top = new QSplitter(Qt::Vertical,main_window);
-    main_middle = new QSplitter(Qt::Vertical,main_top);
-    main_bottom = new QSplitter(Qt::Horizontal,main_middle);
-    main_window_botton_1 = new QPushButton("参数设置",main_bottom);
-    main_window_botton_2 = new QPushButton("定标",main_bottom);
-    main_window_botton_3 = new QPushButton("系统",main_bottom);
-    main_top_exit = new QTextEdit("top",main_top);
-    main_middle_edit = new QTextEdit("middle",main_middle);
-    main_bottom_edit = new QTextEdit("window",main_window);
-    QPalette pal(main_window->palette());
+    main_bottom = new QSplitter(Qt::Vertical,0);
+    main_operations = new QSplitter(Qt::Horizontal,main_bottom);
+    main_operations_details = new QSplitter(Qt::Horizontal,main_bottom);
+    main_main = new QSplitter(Qt::Horizontal,main_bottom);
+    main_main_middle = new QSplitter(Qt::Horizontal,main_main);
+    main_main_right = new QSplitter(Qt::Horizontal,main_main);
+
+    //设定顶端按钮
+    main_operations_details_widget = new QWidget;
+    main_operations_details_layout = new QHBoxLayout;
+    System = new QPushButton("系统管理");
+    Calibration_and_Observation = new QPushButton("定标与观测");
+    Parameter_Setting = new QPushButton("参数设置");
+    System->setFixedSize(100,30);
+    Calibration_and_Observation->setFixedSize(100,30);
+    Parameter_Setting->setFixedSize(100,30);
+    main_operations_details_layout->addWidget(System);
+    main_operations_details_layout->addWidget(Calibration_and_Observation);
+    main_operations_details_layout->addWidget(Parameter_Setting);
+    main_operations_details_layout->setAlignment(Qt::AlignLeft);
+    main_operations_details_layout->setSpacing(30);
+    main_operations_details_widget->setLayout(main_operations_details_layout);
+    main_operations_details->addWidget(main_operations_details_widget);
+
+    //设定次顶端按钮
+    main_operations_Widget = new QWidget();
+    main_operations_layout = new QHBoxLayout();
+    Help = new QPushButton("帮助");
+    Exit = new QPushButton("退出");
+    Help->setFixedSize(100,30);
+    Exit->setFixedSize(100,30);
+    main_operations_layout->addWidget(Help);
+    main_operations_layout->addWidget(Exit);
+    main_operations_layout->setAlignment(Qt::AlignLeft);
+    main_operations_layout->setSpacing(30);
+    main_operations_Widget->setLayout(main_operations_layout);
+    main_operations->addWidget(main_operations_Widget);
+
+    //设定底端显示
+    main_bottom_widget = new QWidget();
+    main_bottom_layout = new QHBoxLayout();
+    edit = new QTextEdit("无法连接CAN设备");
+    main_bottom_layout->addWidget(edit);
+    main_bottom_widget->setLayout(main_bottom_layout);
+    main_bottom->addWidget(main_bottom_widget);
+
+    //设定中部显示
+    main_main_widget = new QWidget;
+    main_main_layout = new QHBoxLayout;
+    top = new QTextEdit("top");
+    main_main_layout->addWidget(top);
+    main_main_widget->setLayout(main_main_layout);
+    main_main->addWidget(main_main_widget);
+
+    main_main_mi = new QTextEdit("hello",main_main_middle);
+    main_main_ri = new QTextEdit("hello",main_main_right);
+    QPalette pal(main_bottom->palette());
     pal.setColor(QPalette::Background,QColor(160,32,240,255));
-    main_window->setStretchFactor(0,17);
-    main_window->setStretchFactor(1,2);
-    main_top->setStretchFactor(0,1);
-    main_top->setStretchFactor(1,8);
-    main_middle->setStretchFactor(0,8);
-    main_middle->setStretchFactor(1,3);
-    main_window->setWindowTitle("client");
-    main_window->setAutoFillBackground(true);
-    main_window->setPalette(pal);
-    main_window->showMaximized();
-    main_window->show();
+    main_bottom->setStretchFactor(0,1);
+    main_bottom->setStretchFactor(1,2);
+    main_bottom->setStretchFactor(2,8);
+    main_bottom->setStretchFactor(3,1);
+    main_main->setStretchFactor(0,1.5);
+    main_main->setStretchFactor(1,5);
+    main_main->setStretchFactor(2,1.5);
+    main_bottom->setWindowTitle("client");
+    main_bottom->setAutoFillBackground(true);
+    main_bottom->setPalette(pal);
+    main_bottom->showMaximized();
+    main_bottom->show();
 }
 mainwindow::~mainwindow()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -5,19 +5,36 @@
 #include <QTextEdit>
 #include <QPalette>
 #include <QColor>
+#include <QWidget>
+#include <QHBoxLayout>
+#include <QRect>
 class mainwindow
 {
 private:
-    QSplitter * main_window;
-    QSplitter * main_top;
-    QSplitter * main_middle;
     QSplitter * main_bottom;
-    QPushButton * main_window_botton_1;
-    QPushButton * main_window_botton_2;
-    QPushButton * main_window_botton_3;
-    QTextEdit * main_top_exit;
-    QTextEdit * main_middle_edit;
-    QTextEdit * main_bottom_edit;
+    QSplitter * main_main;
+    QSplitter * main_operations;
+    QSplitter * main_operations_details;
+    QSplitter * main_main_middle;
+    QSplitter * main_main_right;
+
+    QWidget * main_bottom_widget;
+    QWidget * main_operations_Widget;
+    QWidget * main_operations_details_widget;
+    QWidget * main_main_widget;
+    QHBoxLayout * main_bottom_layout;
+    QHBoxLayout * main_operations_layout;
+    QHBoxLayout * main_operations_details_layout;
+    QHBoxLayout * main_main_layout;
+    QPushButton * System;
+    QPushButton * Calibration_and_Observation;
+    QPushButton * Parameter_Setting;
+    QPushButton * Help;
+    QPushButton * Exit;
+    QTextEdit * main_main_mi;
+    QTextEdit * main_main_ri;
+    QTextEdit  * top;
+    QTextEdit * edit;
 public:
     mainwindow();
     ~mainwindow();
